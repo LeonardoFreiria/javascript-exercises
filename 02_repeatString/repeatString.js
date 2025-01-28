12bae2733c14f6b0b9a5e122f235dcafb193e8c2
Leonardo Freiria <88349331+LeonardoFreiria@users.noreply.github.com>
2025-01-28 07:09:32 -0300

    Solution for exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(phrase, howManyTimes) {
+    if (howManyTimes < 0) {
+        return "ERROR"
+    }
 
+    let phrasesConcatened = "";
+    for (let i = 0; i < howManyTimes; i++) {
+        phrasesConcatened += phrase;
+    }
+    return phrasesConcatened;
 };
 
 // Do not edit below this line
